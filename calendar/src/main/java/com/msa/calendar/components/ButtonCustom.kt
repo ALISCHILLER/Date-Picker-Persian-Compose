b38251cd78f5499c3222d26fa42de2abf4fc5118
SOFTWARE-SV\zar <zartablet2022@gmail.com>
2023-09-16 15:40:40 +0330

    custome bu

---
+++ calendar/src/main/java/com/msa/calendar/components/ButtonCustom.kt
@@ -0,0 +1,19 @@
+package com.msa.calendar.components
+
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ButtonCustom(
+
+){
+}
+
+@Composable
+@Preview
+fun ButtonCustomPreview(){
+    ButtonCustom()
+}
