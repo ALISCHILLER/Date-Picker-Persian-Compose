81bae993f5a963a2f359582d4773b44fd8d42324
SOFTWARE-SV\zar <zartablet2022@gmail.com>
2023-11-16 09:45:29 +0330

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,8 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-
     implementation(project(":calendar"))
 }
+
+
+
