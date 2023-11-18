c1687f8634f546aa9c6cd58dc5c659043c5fb654
SOFTWARE-SV\zar <zartablet2022@gmail.com>
2023-11-18 13:20:43 +0330

    publishing

--- calendar/build.gradle.kts
+++ calendar/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    `maven-publish`
+    id("maven-publish")
+    id("signing")
 }
 
 android {
@@ -39,6 +40,7 @@ android {
         kotlinCompilerExtensionVersion = "1.4.3"
     }
 
+
 }
 
 dependencies {
@@ -49,8 +51,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
@@ -60,4 +60,65 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation(kotlin("stdlib"))
 }
+
+
+
+publishing {
+    repositories {
+        maven {
+            name = "PersionCalendar"
+            setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
+            credentials {
+                username = project.findProperty("mavenCentralUsername")?.toString() ?: System.getenv("MAVEN_USERNAME")
+                password = project.findProperty("mavenCentralPassword")?.toString() ?: System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "co.ALISCHILLER"
+            artifactId = "persioncalendar"
+            version = "0.1.1"
+            afterEvaluate {
+                from(components["release"])
+            }
+
+            pom {
+                name.set("PersionCalendar")
+                description.set("An extensible persion calendar for the jetpack composition system.")
+                url.set("https://github.com/ALISCHILLER/Date-Picker-Persian-Compose")
+                licenses {
+                    license {
+                        name.set(" GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007")
+                        url.set("https://www.gnu.org/licenses/gpl-3.0.en.html#license-text")
+                        distribution.set("https://www.gnu.org/licenses/gpl-3.0.en.html#license-text")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("ALISCHILLER")
+                        name.set("ALI Soleimani")
+                        url.set("https://github.com/ALISCHILLER")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/ALISCHILLER/Date-Picker-Persian-Compose")
+                    connection.set("scm:git:git://ALISCHILLER/Date-Picker-Persian-Compose.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:codeandtheory/YCharts.git")
+                }
+            }
+        }
+    }
+}
+signing {
+    useInMemoryPgpKeys(
+        project.findProperty("signing.keyId")?.toString() ?: System.getenv("SIGNINGKEY"),
+        project.findProperty("signing.InMemoryKey")?.toString() ?: System.getenv("MEMORY_KEY"),
+        project.findProperty("signing.password")?.toString()?:System.getenv("SIGNINGPASSWORD")
+    )
+    sign(publishing.publications)
+}
+
+
